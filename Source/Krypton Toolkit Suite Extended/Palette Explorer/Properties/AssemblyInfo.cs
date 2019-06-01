6f1e81ccc0d54974cac5f834c99d3cd8f95e70c8
Peter Wagner <peterwagner@live.co.uk>
2019-06-01 10:30:58 +0100

    * Build 1030

--- Source/Krypton Toolkit Suite Extended/Palette Explorer/Properties/AssemblyInfo.cs
+++ Source/Krypton Toolkit Suite Extended/Palette Explorer/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.451.619.0")]
-[assembly: AssemblyFileVersion("5.451.619.0")]
+[assembly: AssemblyVersion("5.451.753.0")]
+[assembly: AssemblyFileVersion("5.451.753.0")]
